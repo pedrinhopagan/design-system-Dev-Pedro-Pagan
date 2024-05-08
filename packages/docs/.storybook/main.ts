e34ce08698df01ab80eadae2c02d27bd181c6a28
Pedro Pagan <pedroantoniobonaldo@gmail.com>
2024-05-08 17:52:26 -0300

    Fix add subpath do production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -28,7 +28,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/design-system-dev-pedro-pagan'
     }
     return config;
   },
