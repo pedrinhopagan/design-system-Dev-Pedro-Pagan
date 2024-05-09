fa2a91ec83cd243aca7331ae3ce189682b26e2c8
Pedro Pagan <pedroantoniobonaldo@gmail.com>
2024-05-09 17:09:32 -0300

    Trying rocketseat preview

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,13 +1,14 @@
-import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming'
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-      },
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
-};
-
-export default preview;
+  docs: {
+    theme: themes.dark,
+  }
+}
